3badfee26c9913af9159a8bddf91ac2efaaae9e5
Steve Kean <ppsel03@gmail.com>
2025-05-19 21:05:17 -0500

    added keystore and signed apk see csproj

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,16 @@
-﻿namespace AviationApp;
+﻿using Microsoft.Maui.Controls;
+
+namespace AviationApp;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    public App()
+    {
+        InitializeComponent();
+    }
 
-		MainPage = new AppShell();
-	}
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        return new Window(new MainPage());
+    }
 }
